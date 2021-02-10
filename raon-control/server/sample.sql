284fa7d77c5d8aeb2d0e9187304a2c1731dd3b28
scwook <scwook@gmail.com>
2021-02-10 09:48:29 +0900

    sample update

--- raon-control/server/sample.sql
+++ raon-control/server/sample.sql
@@ -7,13 +7,13 @@ INSERT INTO schedule values
 
 INSERT INTO detail values
 (1,'ECR Repeatability Test'),
-(1,'ECR Beam Current Test')
-(2,'Beam Transport to LEBT')
-(2,'Beam Transport to MEBT')
-(3,'Timing and FPS Interlock Test')
-(4,'Pulse Beam Test')
-(4,'250us Pulse Beam to MEBT')
-(4,'Beam Steering Test')
+(1,'ECR Beam Current Test'),
+(2,'Beam Transport to LEBT'),
+(2,'Beam Transport to MEBT'),
+(3,'Timing and FPS Interlock Test'),
+(4,'Pulse Beam Test'),
+(4,'250us Pulse Beam to MEBT'),
+(4,'Beam Steering Test'),
 (5,'Main Control Room Test');
 
 INSERT INTO shifter values
@@ -21,34 +21,34 @@ INSERT INTO shifter values
 (1,'Kim, Gi Dong','Beam Transport',4,0,0),
 (1,'Kwon, Jangwon','Beam Transport',4,1,0),
 (1,'Lim, EunHun','Beam Transport',4,2,0),
-(1,'Kim, Hyung Jin','Beam Transport',4,0,0);
-(1,'Heo, JeongIl','Beam ransport',4,4,0);
-(1,'Mun, Seokho','Beam Transport',4,5,0);
+(1,'Kim, Hyung Jin','Beam Transport',4,0,0),
+(1,'Heo, JeongIl','Beam ransport',4,4,0),
+(1,'Mun, Seokho','Beam Transport',4,5,0),
 (1, 'Park, Burm Sik','Beam Transport',2,6,0),
 (1,'Son, Changwook','Accelerator Control',1,1,0),
 (1,'Park, MiJeong','Accelerator Control',1,2,0),
-(1,'Lee, SangIl','Accelerator Control',1,3,0);
+(1,'Lee, SangIl','Accelerator Control',1,3,0),
 (1, 'Seol, Kyung Tae','SCRF',3,1,0),
 (1, 'Lee, DoYoon','SCRF',3,2,0),
-(1, 'Son, KiTaek','SCRF',3,3,0);
+(1, 'Son, KiTaek','SCRF',3,3,0),
 
 (2,'Chung, Yeon Sei','Beam Transport',4,3,1),
 (2,'Kim, Gi Dong','Beam Transport',4,0,0),
 (2,'Kwon, Jangwon','Beam Transport',4,1,0),
 (2,'Lim, EunHun','Beam Transport',4,2,0),
-(2,'Kim, Hyung Jin','Beam Transport',4,0,0);
-(2,'Heo, JeongIl','Beam ransport',4,4,0);
-(2,'Mun, Seokho','Beam Transport',4,5,0);
+(2,'Kim, Hyung Jin','Beam Transport',4,0,0),
+(2,'Heo, JeongIl','Beam ransport',4,4,0),
+(2,'Mun, Seokho','Beam Transport',4,5,0),
 (2, 'Park, Burm Sik','Beam Transport',2,6,0),
 (2,'Son, Changwook','Accelerator Control',1,1,0),
 (2,'Park, MiJeong','Accelerator Control',1,2,0),
-(2,'Lee, SangIl','Accelerator Control',1,3,0);
+(2,'Lee, SangIl','Accelerator Control',1,3,0),
 (2, 'Seol, Kyung Tae','SCRF',3,1,0),
 (2, 'Lee, DoYoon','SCRF',3,2,0),
-(2, 'Son, KiTaek','SCRF',3,3,0);
+(2, 'Son, KiTaek','SCRF',3,3,0),
 
 (3,'Son, Changwook','Accelerator Control',1,1,0),
-(3,'Lee, SangIl','Accelerator Control',1,3,1);
+(3,'Lee, SangIl','Accelerator Control',1,3,1),
 (3,'Choi, Yong Jun','Accelerator Control',1,2,0),
 
 (4,'Chung, Yeon Sei','Beam Transport',4,3,1),
@@ -61,17 +61,17 @@ INSERT INTO shifter values
 (4, 'Park, Burm Sik','Beam Transport',2,6,0),
 (4,'Son, Changwook','Accelerator Control',1,1,0),
 (4,'Park, MiJeong','Accelerator Control',1,2,0),
-(4,'Lee, SangIl','Accelerator Control',1,3,0);
+(4,'Lee, SangIl','Accelerator Control',1,3,0),
 (4, 'Seol, Kyung Tae','SCRF',3,1,0),
 (4, 'Lee, DoYoon','SCRF',3,2,0),
-(4, 'Son, KiTaek','SCRF',3,3,0);
+(4, 'Son, KiTaek','SCRF',3,3,0),
 
 (5,'Chung, Yeon Sei','Beam Transport',4,3,1),
 (5,'Kim, Gi Dong','Beam Transport',4,0,0),
 (5,'Woo, Hyung Joo','Beam Transport',4,0,0),
 (5,'Kwon, Jangwon','Beam Transport',4,1,0),
 (5,'Lim, EunHun','Beam Transport',4,2,0),
-(5,'Kim, Hyung Jin','Beam Transport',4,0,0);
-(5,'Heo, JeongIl','Beam ransport',4,4,0);
-(5,'Mun, Seokho','Beam Transport',4,5,0);
-(5,'Park, MiJeong','Accelerator Control',1,2,0),
+(5,'Kim, Hyung Jin','Beam Transport',4,0,0),
+(5,'Heo, JeongIl','Beam ransport',4,4,0),
+(5,'Mun, Seokho','Beam Transport',4,5,0),
+(5,'Park, MiJeong','Accelerator Control',1,2,0);
