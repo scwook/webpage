6be50b9584f266ed4f4d7db0fd62cc57d4980e90
scwook <scwook@gmail.com>
2021-02-11 18:28:13 +0900

    inventory db test

--- raon-control/server/inventory.py
+++ raon-control/server/inventory.py
@@ -12,14 +12,14 @@ CORS(app)
 
 @app.route('/inventory', methods=['GET', 'POST'])
 def get_inventory():
-    #conn = pymysql.connect(host='localhost', user='scwook', password='qwer1234', db='inventory', charset='utf8')
-    #inventory = conn.cursor()
-    #temp = {'assetNum':'20130619', 'date':'2013-06', 'deviceName':'IPhone', 'manager':'Son, Changwook', 'fileName':'picture.jpg'}
-    #insertQuery = 'INSERT INTO asset_list VALUES(NULL,' + temp['assetNum'] + "," + temp['date'] + "," + temp['deviceName'] + "," + temp['manager'] + "," + temp['fileName'] + ")"
-    #return insertQuery
+    conn = pymysql.connect(host='localhost', user='scwook', password='qwer1234', db='inventory', charset='utf8')
+    inventory = conn.cursor()
+    temp = {'assetNum':'20130619', 'date':'2013-06', 'deviceName':'IPhone', 'manager':'Son, Changwook', 'fileName':'picture.jpg'}
+    insertQuery = 'INSERT INTO asset_list VALUES(NULL,' + temp['assetNum'] + "," + temp['date'] + "," + temp['deviceName'] + "," + temp['manager'] + "," + temp['fileName'] + ")"
+    return insertQuery
     # data = request.get_json()
 
-    return 'aaa'
+    #return 'aaa'
 
 if __name__ == "__main__":
     app.run(host="192.168.0.105", port="8080")
