44a66322e2dda1a9992d553f9f3190d094fd2c1e
scwook <scwook@gmail.com>
2021-02-23 07:55:07 +0900

    change inventory name

--- raon-control/server/inventory.py
+++ raon-control/server/inventory.py
@@ -12,52 +12,114 @@ app = Flask(__name__)
 CORS(app)
 
 UPLOAD_FOLDER = '/home/scwook/flask/upload'
+DB_HOST = 'localhost'
+DB_USER = 'scwook'
+DB_PASSWORD = 'qwer1234'
+DB_DATABASE = 'inventory'
 
-@app.route('/inventory/insert', methods=['GET', 'POST'])
+@app.route('/inventory/insert', methods=['POST'])
 def set_asset_list():
-    conn = pymysql.connect(host='localhost', user='scwook', password='qwer1234', db='inventory', charset='utf8')
+    conn = pymysql.connect(host=DB_HOST, user=DB_USER, password=DB_PASSWORD, db=DB_DATABASE, charset='utf8')
     inventory = conn.cursor()
     retrieve = conn.cursor()
-    jsonData = request.get_json()
+    # jsonData = request.get_json()
+    formData = request.form
 
-    assetNum = "'" + jsonData['assetNum'] + "'"
-    date = "'" + jsonData['date'] + "'"
-    deviceName = "'" + jsonData['deviceName'] + "'"
-    manager = "'" + jsonData['manager'] + "'"
-    location = "'" + jsonData['location'] + "'"
-    fileName = "'" + jsonData['fileName'] + "'"
+    assetNumber = "'" + formData['assetNumber'] + "'"
+    date = "'" + formData['assetDate'] + "'"
+    deviceName = "'" + formData['assetName'] + "'"
+    manager = "'" + formData['assetManager'] + "'"
+    location = "'" + formData['assetLocation'] + "'"
 
-    retrieveQuery = 'SELECT * FROM asset_list WHERE AssetNumber LIKE ' + assetNum
+    file = request.files['file']
+    #sercureFileName = secure_filename(file.filename)
+    fileName = "'" + file.filename + "'"
+   
+    retrieveQuery = 'SELECT * FROM asset_list WHERE AssetNumber LIKE ' + assetNumber
     retrieve.execute(retrieveQuery)
     result = retrieve.fetchall()
 
     if len(result):
         return 'overlap'
 
-    insertQuery = 'INSERT INTO asset_list(AssetNumber,Date,DeviceName,Manager,Location,FileName) VALUES(' + assetNum + "," + date + "," + deviceName + "," + manager + "," + location + "," + fileName + ")"
+    insertQuery = 'INSERT INTO asset_list(AssetNumber,Date,DeviceName,Manager,Location,FileName) VALUES(' + assetNumber + "," + date + "," + deviceName + "," + manager + "," + location + "," + fileName + ")"
 
     inventory.execute(insertQuery)
     conn.commit()
+
+    retrieveQuery = 'SELECT max(ID) FROM asset_list'
+    inventory.execute(retrieveQuery)
+    lastID = inventory.fetchone()[0]
+
     conn.close()
 
-    #return json.dumps(jsonData)
-    return 'success'
+    folder = UPLOAD_FOLDER + '/' + str(lastID)
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+        
+    if file:
+        file.save(os.path.join(folder, file.filename))
+        return 'success'
 
-@app.route('/inventory/update', methods=['GET', 'POST'])
+    else:
+        return 'fail'
+
+   # return 'OK'
+  #return json.dumps(jsonData)
+
+@app.route('/inventory/update', methods=['POST'])
 def update_asset_list():
-    conn = pymysql.connect(host='localhost', user='scwook', password='qwer1234', db='inventory', charset='utf8')
+    conn = pymysql.connect(host=DB_HOST, user=DB_USER, password=DB_PASSWORD, db=DB_DATABASE, charset='utf8')
     inventory = conn.cursor()
-    updateQuery = 'UPDATE asset_list SET fileName = "test.jpg" WHERE ID = 1'
+    retrieve = conn.cursor()
+
+    formData = request.form
+
+    referenceNum = "'" + formData['reference'] + "'"
+    assetNumber = "'" + formData['assetNumber'] + "'"
+    date = "'" + formData['assetDate'] + "'"
+    deviceName = "'" + formData['assetName'] + "'"
+    manager = "'" + formData['assetManager'] + "'"
+    location = "'" + formData['assetLocation'] + "'"
+
+    file = request.files['file']
+    fileName = "'" + file.filename + "'"
+
+    retrieveQuery = 'SELECT * FROM asset_list WHERE AssetNumber LIKE ' + assetNumber
+    retrieve.execute(retrieveQuery)
+    result = retrieve.fetchall()
+
+    for x in retrieve:
+        if x[1] != assetNumber:
+            return 'overlap'
+
+    updateQuery = 'UPDATE asset_list SET AssetNumber=' + assetNumber + ',Date=' + date + ',DeviceName=' + deviceName + ',Manager=' + manager + ',Location=' + location + ",FileName=" + fileName + ' WHERE AssetNumber=' + referenceNum
 
     inventory.execute(updateQuery)
     conn.commit()
+    
+    retrieveQuery = 'SELECT ID FROM asset_list WHERE AssetNumber=' + assetNumber
+    inventory.execute(retrieveQuery)
+    ID = inventory.fetchone()[0]
+
     conn.close()
+
+    folder = UPLOAD_FOLDER + '/' + str(ID)
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+        
+    if file:
+        file.save(os.path.join(folder, file.filename))
+        return 'success'
+
+    else:
+        return 'fail'
 
 @app.route('/inventory/retrieve/<asset>')
 def get_asset_list(asset):
-    conn = pymysql.connect(host='localhost', user='scwook', password='qwer1234', db='inventory', charset='utf8')
+    conn = pymysql.connect(host=DB_HOST, user=DB_USER, password=DB_PASSWORD, db=DB_DATABASE, charset='utf8')
     inventory = conn.cursor()
-    inventoryArray = []
+    assetListArray = []
 
     search = asset.replace("*", "%")
     retrieveQuery = 'SELECT * FROM asset_list WHERE AssetNumber LIKE ' + "'" + search + "'"
@@ -65,48 +127,12 @@ def get_asset_list(asset):
     inventory.execute(retrieveQuery)
 
     for x in inventory:
-        inventoryDic = {'id':x[0], 'assetNum':x[1], 'date':x[2], 'deviceName':x[3], 'manager':x[4], 'location':x[5], 'fileName':x[6]}
-        inventoryArray.append(inventoryDic)
+        inventoryDic = {'id':x[0], 'assetNumber':x[1], 'date':x[2], 'deviceName':x[3], 'manager':x[4], 'location':x[5], 'fileName':x[6]}
+        assetListArray.append(inventoryDic)
     
     conn.close()
 
-    return json.dumps(inventoryArray, ensure_ascii=False)
-
-@app.route('/inventory/upload', methods=['GET', 'POST'])
-def file_save():
-    conn = pymysql.connect(host='localhost', user='scwook', password='qwer1234', db='inventory', charset='utf8')
-    inventory = conn.cursor()
-    
-    retrieveQuery = 'SELECT max(ID) FROM asset_list'
-    inventory.execute(retrieveQuery)
-    lastID = inventory.fetchone()[0]
-
-    folder = UPLOAD_FOLDER + '/' + str(lastID)
-
-    if not os.path.exists(folder):
-        os.makedirs(folder)
-
-    if request.method == 'POST':
-        # check if the post request has the file part
-        #if 'file' not in request.files:
-        #    flash('No file part')
-        #    return redirect(request.url)
-
-        file = request.files['file']
-
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        #if file.filename == '':
-        #    flash('No selected file')
-        #    return redirect(request.url)
-        
-        if file:
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(folder, filename))
-            return 'success'
-        else:
-            return 'fail'
-
+    return json.dumps(assetListArray, ensure_ascii=False)
 
 if __name__ == "__main__":
-    app.run(host="localhost", port="8080")
+    app.run(host="192.168.68.126", port="8080")
