5e85f5fcdae31f8ca33cc51773c601fb9ce763ea
scwook <scwook@gmail.com>
2021-02-23 17:08:43 +0900

    epics flask test

--- pyepics/pyepicsget.py
+++ pyepics/pyepicsget.py
@@ -1,8 +1,10 @@
 import time
+import json
+
 from pvaccess import *
 
 c = MultiChannel(['scwookHost:ai1','scwookHost:ai2','scwookHost:ai3'], ProviderType.CA)
 
 while 1:
-	print c.get().toJSON(False)
+	print c.get('field(value,alarm)').toJSON(False)
 	time.sleep(1)
--- pyepics/test.py
+++ pyepics/test.py
@@ -5,9 +5,9 @@ def echo(x):
     print(x.toJSON(False))
 
 c = Channel('scwookHost:ai1', ProviderType.CA)
-#c.monitor(echo)
-c.subscribe('echo', echo)
-c.startMonitor()
+c.monitor(echo)
+#c.subscribe('echo', echo)
+#c.startMonitor()
 
 while 1:
     time.sleep(1)
