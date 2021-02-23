dc9107819d8701629e662262e40d1988c71b94a5
scwook <scwook@gmail.com>
2021-02-23 21:21:35 +0900

    epics flask test

--- pyepics/pyepicsget.py
+++ pyepics/pyepicsget.py
@@ -4,7 +4,8 @@ import json
 from pvaccess import *
 
 c = MultiChannel(['scwookHost:ai1','scwookHost:ai2','scwookHost:ai3'], ProviderType.CA)
-
+count = 0
 while 1:
-	print c.get('field(value,alarm)').toJSON(False)
+	print count
+        count += 1
 	time.sleep(1)
--- raon-control/server/epics.py
+++ raon-control/server/epics.py
@@ -6,17 +6,40 @@ from pvaccess import *
 from flask import Flask, jsonify
 from flask_cors import CORS
 
-mChannel = MultiChannel(['scwookHost:ai1','scwookHost:ai2','scwookHost:ai3'], ProviderType.CA)
-data = {'a':'b'}
+jsonData = dict()
+test = {'a':'b'}
 
+class ChannelMonitor:
+    def __init__(self, name):
+        self.name = name
+
+    def monitor(self, data):
+        jsonData[self.name] = data.toJSON(False)
+	print(jsonData[self.name])
+
+### Read Channel List
+pvList = list(line.strip() for line in open('pvList'))
+
+channelList = list()
+monitoringList = list()
+
+index = 0
+for name in pvList:
+        channelList.append(Channel(name, ProviderType.CA))
+        monitoringList.append(ChannelMonitor(name))
+
+        channelList[index].monitor(monitoringList[index].monitor, 'field(value,alarm)')
+        index += 1
+
+
+### Flask App
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/')
 def get_data():
-    val = mChannel.get('field(value,alarm)')
-#    print(dict(val))
-    return json.dumps(data)
+        return json.dumps(jsonData)
+
 
 if __name__ == "__main__":
-    app.run(host="192.168.68.126", port="8080")
+    app.run(host="192.168.0.105", port="8080")
