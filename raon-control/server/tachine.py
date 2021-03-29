124dd55cb54e23c82fb341d9b1303e1418a7a3c9
scwook <scwook@gmail.com>
2021-03-29 09:15:23 +0900

    retrieve event and snapshot

--- raon-control/server/tachine.py
+++ raon-control/server/tachine.py
@@ -15,10 +15,10 @@ DB_USER = 'ctrluser'
 DB_PASSWORD = 'qwer1234'
 DB_DATABASE = 'tachine'
 
-@app.route('/tachine/event', methods=['POST'])
+@app.route('/timeleap/event', methods=['POST'])
 def create_event():
     conn = pymysql.connect(host=DB_HOST, user=DB_USER, password=DB_PASSWORD, db=DB_DATABASE, charset='utf8')
-    tachine = conn.cursor()
+    timeleap = conn.cursor()
 
     jsonData = request.get_json()
 
@@ -27,68 +27,117 @@ def create_event():
     eventTitle = "'" + jsonData['title'] + "'"
 
     insertEventQuery = 'INSERT INTO event(date,title) values(' + dateNow + "," + eventTitle + ")"
-    tachine.execute(insertEventQuery)
+    timeleap.execute(insertEventQuery)
     conn.commit()
 
     lastEventQuery = 'SELECT max(eventid) FROM event'
-    tachine.execute(lastEventQuery)
-    lastEventID = str(tachine.fetchone()[0])
-    lastEventIDStr = "'" + str(tachine.fetchone()[0]) + "'"
+    timeleap.execute(lastEventQuery)
+    lastEventID = str(timeleap.fetchone()[0])
+    lastEventIDStr = "'" + str(timeleap.fetchone()[0]) + "'"
 
     # Insert new snapshot
     snapshotDescription = "'" + jsonData['description'] + "'"
 
     insertSnapshotQuery = 'INSERT INTO snapshot_info(description,eventid) values(' + snapshotDescription + "," + lastEventIDStr + ")"
-    tachine.execute(insertSnapshotQuery)
+    timeleap.execute(insertSnapshotQuery)
     conn.commit()
 
     lastSnapshotQuery = 'SELECT max(snapshotid) FROM snapshot_info'
 
-    tachine.execute(lastSnapshotQuery)
-    lastSnapshotID = str(tachine.fetchone()[0])
+    timeleap.execute(lastSnapshotQuery)
+    lastSnapshotID = str(timeleap.fetchone()[0])
     
-    # Create snapshot table
+    # Create new snapshot table
     createTableQuery = 'CREATE TABLE snapshot_' + lastSnapshotID + '(pvname varchar(255) NOT NULL, value varchar(255))'
-    tachine.execute(createTableQuery)
+    timeleap.execute(createTableQuery)
     conn.commit()
 
     conn.close()
 
-    tachineDic = {'eventid' : lastEventID, 'snapshotid' : lastSnapshotID}
+    timeleapDic = {'eventid' : lastEventID, 'snapshotid' : lastSnapshotID}
 
-    return json.dumps(tachineDic)
+    return json.dumps(timeleapDic)
 
-@app.route('/tachine/retrieve/snapshot/<id>')
-def get_asset_list(id):
+
+@app.route('/timeleap/snapshot', methods=['POST'])
+def create_snapshot(id):
     conn = pymysql.connect(host=DB_HOST, user=DB_USER, password=DB_PASSWORD, db=DB_DATABASE, charset='utf8')
-    tachine = conn.cursor()
+    timeleap = conn.cursor()
 
-    return 'OK'
+    jsonData = request.get_json()
 
+    # Insert new snapshot
+    eventID = "'" + jsonData['eventid'] + "'"
+    snapshotDescription = "'" + jsonData['description'] + "'"
+    
+    insertSnapshotQuery = 'INSERT INTO snapshot_info(description,eventid) values(' + snapshotDescription + "," + eventID + ")"
+    timeleap.execute(insertSnapshotQuery)
+    conn.commit()
+    
+    lastSnapshotQuery = 'SELECT max(snapshotid) FROM snapshot_info'
 
-@app.route('/tachine/snapshot/<snapshotData>', methods=['POST'])
-def create_snapshot(snapshotData):
-    conn = pymysql.connect(host=DB_HOST, user=DB_USER, password=DB_PASSWORD, db=DB_DATABASE, charset='utf8')
-    tachine = conn.cursor()
-
-    eventID = snapshotData['eventid']
-    snapshotDescription = snapshotData['description']
-
-    insertQuery = 'INSERT INTO snapshot_info(description,eventid) values(' + snapshotDescription + "," + eventID + ")"
-    tachine.execute(insertQuery)
+    timeleap.execute(lastSnapshotQuery)
+    lastSnapshotID = str(timeleap.fetchone()[0])
+    
+    # Create new snapshot table
+    createTableQuery = 'CREATE TABLE snapshot_' + lastSnapshotID + '(pvname varchar(255) NOT NULL, value varchar(255))'
+    timeleap.execute(createTableQuery)
     conn.commit()
 
-    lastSnapshotQuery = 'SELECT max(snapshotid) FROM snapshot_info'
-    tachine.execute(lastSnapshotQuery)
-    lastSnapshotID = tachine.fetchone()[0]
+    conn.close()
 
-    createTableQuery = 'CREATE TABLE snapshot_' + lastSnapshotID + '(pvname varchar(255) NOT NULL, value varchar(255))'
-    tachine.execute(createTableQuery)
+    timeleapDic = {'snapshotid': lastSnapshotID}
 
-    tachineDic = {'snapshotid' : lastSnapshotID}
+    return json.dumps(timeleapDic)
 
-    return json.dumps(tachineDic)
+@app.route('/timeleap/retrieve/event', methods=['GET'])
+def get_event_list():
+    conn = pymysql.connect(host=DB_HOST, user=DB_USER, password=DB_PASSWORD, db=DB_DATABASE, charset='utf8')
+    timeleap = conn.cursor()
 
+    retrieveQuery = 'SELECT * FROM event'
+    timeleap.execute(retrieveQuery)
+
+    eventArray = []
+    for x in timeleap:
+        eventDic = {'eventid':x[0], 'date':x[1], 'title':x[2]}
+        eventArray.append(eventDic)
+
+    return json.dumps(eventArray, ensure_ascii=False)
+
+@app.route('/timeleap/retrieve/snapshot', methods=['GET'])
+def get_snapshot_list():
+    conn = pymysql.connect(host=DB_HOST, user=DB_USER, password=DB_PASSWORD, db=DB_DATABASE, charset='utf8')
+    timeleap = conn.cursor()
+
+    retrieveQuery = 'SELECT * FROM snapshot_info'
+    timeleap.execute(retrieveQuery)
+
+    snapshotArray = []
+    for x in timeleap:
+        snapshotDic = {'snapshotid':x[0], 'description':x[1], 'eventid':x[2]}
+        snapshotArray.append(snapshotDic)
+
+    return json.dumps(snapshotArray, ensure_ascii=False)
+
+@app.route('/timeleap/retrieve/snapshot/record/<id>', methods=['GET'])
+def get_record_list(id):
+    conn = pymysql.connect(host=DB_HOST, user=DB_USER, password=DB_PASSWORD, db=DB_DATABASE, charset='utf8')
+    timeleap = conn.cursor()
+
+    snapshotID = str(id)
+
+    retrieveQuery = 'SELECT * FROM snapshot_' + snapshotID
+    timeleap.execute(retrieveQuery)
+
+    recordArray = []
+    for x in timeleap:
+        recordDic = {'pvname':x[0], 'value':x[1]}
+        recordArray.append(recordDic)
+
+    conn.close()
+
+    return json.dumps(recordArray, ensure_ascii=False)
 
 if __name__ == "__main__":
     app.run(host=SERVER_ADDR, port="9013")
