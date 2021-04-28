c964fa7d11fc20c7ac777ffa6242c865a253846e
scwook <scwook@gmail.com>
2021-04-28 16:27:13 +0900

    MR

--- pyepics/test.py
+++ pyepics/test.py
@@ -1,13 +1,33 @@
 import time
 from pvaccess import *
 
-def echo(x):
-    print(x.toJSON(False))
+class ChannelMonitor:
+    def __init__(self, snapshotid):
+        self.snapshotid = snapshotid
 
-c = Channel('scwookHost:ai1', ProviderType.CA)
-c.monitor(echo)
-#c.subscribe('echo', echo)
-#c.startMonitor()
+    def connectionChannel(self, pvname):
+        self.pvname = pvname
+        self.ch = Channel(pvname, ProviderType.CA)
+        self.ch.setConnectionCallback(self.isConnected)
 
+    def isConnected(self, status):
+        print(self.snapshotid, self.pvname, status)
+
+d = None
+count = 0
 while 1:
+    print(count)
+
+    if count == 1:
+        d = ChannelMonitor(1)
+        d.connectionChannel('out')
+
+    if count == 5:
+        del f
+    
+    if count == 15:
+        d = ChannelMonitor(1)
+        d.connectionChannel('out')
+    
+    count += 1
     time.sleep(1)
